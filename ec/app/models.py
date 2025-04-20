a522cd65ac111b1db120cd98af6d6161ac13dea3
Brandon0304 <bortiz_194@cue.edu.co>
2025-04-20 01:36:29 -0500

    Marketplace PharmaPlus

---
+++ ec/app/models.py
@@ -0,0 +1,110 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+STATE_CHOICES = (
+    ('Amazonas', 'Amazonas'),
+    ('Antioquia', 'Antioquia'),
+    ('Arauca', 'Arauca'),
+    ('Atlántico', 'Atlántico'),
+    ('Bolívar', 'Bolívar'),
+    ('Boyacá', 'Boyacá'),
+    ('Caldas', 'Caldas'),
+    ('Caquetá', 'Caquetá'),
+    ('Casanare', 'Casanare'),
+    ('Cauca', 'Cauca'),
+    ('Cesar', 'Cesar'),
+    ('Chocó', 'Chocó'),
+    ('Córdoba', 'Córdoba'),
+    ('Cundinamarca', 'Cundinamarca'),
+    ('Guainía', 'Guainía'),
+    ('Guaviare', 'Guaviare'),
+    ('Huila', 'Huila'),
+    ('La Guajira', 'La Guajira'),
+    ('Magdalena', 'Magdalena'),
+    ('Meta', 'Meta'),
+    ('Nariño', 'Nariño'),
+    ('Norte de Santander', 'Norte de Santander'),
+    ('Putumayo', 'Putumayo'),
+    ('Quindío', 'Quindío'),
+    ('Risaralda', 'Risaralda'),
+    ('San Andrés y Providencia', 'San Andrés y Providencia'),
+    ('Santander', 'Santander'),
+    ('Sucre', 'Sucre'),
+    ('Tolima', 'Tolima'),
+    ('Valle del Cauca', 'Valle del Cauca'),
+    ('Vaupés', 'Vaupés'),
+    ('Vichada', 'Vichada'),
+)
+
+CATEGORY_CHOICES=(
+    ('MD', 'Medicamentos'),
+    ('CN', 'Condones'),
+    ('VS', 'Vitaminas y suplementos'),
+    ('CP', 'Cuidado personal'),
+    ('PB', 'Para bebés'),
+    ('CM', 'Cosméticos'),
+    ('SN', 'Snacks'),
+)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default='')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    product_image = models.ImageField(upload_to='product')
+    def __str__(self):
+        return self.title
+    
+
+class Customer(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    
+    @property
+    def total_cost(self):
+        return self.quantity * self.products.discounted_price
+
+STATUS_CHOICES = (
+    ('Aceptado', 'Aceptado'),
+    ('Empacado', 'Empacado'),
+    ('En Camino', 'En Camino'),
+    ('Entregado', 'Entregado'),
+    ('Cancelado', 'Cancelado'),
+    ('Pendiente', 'Pendiente'),
+)
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    razorpay_order_id = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_payment_status = models.CharField(max_length=100, blank=True, null=True)
+    razorpay_payment_id = models.CharField(max_length=100, blank=True, null=True)
+    paid = models.BooleanField(default=False)
+
+class OrderPlaced(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    ordered_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='Pending')
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE, default="")
+    
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
